ecb8a134d085041e09aefdab18ce673fc58b6172
Geraldo <geraldomilholo@MacBook-Air-de-Geraldo.local>
2017-05-09 12:23:50 -0300

    geiser

--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
-var express = require('express'),
+let fs = require('fs');
+let express = require('express'),
     app = express();
+
+
+// configurar qual templating engine usar. Sugestão: hbs (handlebars) -- Dica: app.set('view engine', '???');
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views')
+
+// EXERCÍCIO 1 -- configurar para servir os arquivos estáticos da pasta "client" -- Dica: 1 linha de código
+app.use('/', express.static(__dirname + '/../client'));
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
-var db = {
+let db = {
+	jogadores: JSON.parse(fs.readFileSync(__dirname + '/data/jogadores.json', 'utf-8')),
+	jogosPorJogador: JSON.parse(fs.readFileSync(__dirname + '/data/jogosPorJogador.json', 'utf-8'))
 };
-
-
-// configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???');
-
 
 // EXERCÍCIO 2
 // definir rota para página inicial --> renderizar a view index, usando os
@@ -18,16 +24,25 @@ var db = {
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json
 
+app.get('/', function (req, res) {
+	res.render('index', db.jogadores);
+});
+
+
+
+
+
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter umas 15 linhas de código
 
+// Abrir servidor na porta 3000 -- Dica: 1-3 linhas de código
+let server = app.listen(3000, function () {
+	
+	let host = server.address().address;
+  	let port = server.address().port;
+  	console.log('Listening at http://%s:%s', host, port);
 
-// EXERCÍCIO 1
-// configurar para servir os arquivos estáticos da pasta "client"
-// dica: 1 linha de código
-
-// abrir servidor na porta 3000
-// dica: 1-3 linhas de código
+});
